76538dd71f74e0152f3389f65043af1eeaaca689
Matheus_Baptista <136763434+Matheus-Baptista27@users.noreply.github.com>
2024-05-23 16:03:48 -0300

    primeirocommit

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -26,9 +26,11 @@ class EventController extends Controller
         $event = new Event;
 
         $event->title = $request->title;
+        $event->date = $request->date;
         $event->city = $request->city;
         $event->private = $request->private;
         $event->description = $request->description;
+        $event->items = $request->items;
     
         // image upload
         if($request->hasFile('image') && $request->file('image')->isValid()) {
@@ -50,4 +52,12 @@ class EventController extends Controller
         return redirect('/')->with('msg', 'Evento criado com sucesso!');
 
     }
+
+    public function show($id) {
+
+        $event = Event::findOrFail($id);
+
+        return view('events.show', ['event' => $event]);
+    }
+
 }
